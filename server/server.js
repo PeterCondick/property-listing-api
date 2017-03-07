f51eb17e7d00ebf88c908d56c9ab05ef7f86e641
PeterCondick <pcondick@Peters-MacBook-Pro.local>
2017-03-07 16:53:34 +1100

    updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
+const {ObjectID} = require('mongodb');
+
+var {Property} = require('property-listing-models');
+const PropertyListingServices = require('property-listing-services');
 
 var app = express();
 
@@ -10,41 +16,123 @@ const port = 3000;
 
 app.get('/properties', (req, res) => {
 
-  res.send([{
-    data: 'some dummy data'
-  },
-  {
-    data: 'more dummy data'
-  }]);
-});
-
-app.get('/properties/:id', (req, res) => {
-  res.send({
-    id: req.params.id,
-    data: 'some dummy data'
+  Property.find().then((properties) => {
+    res.send({
+      properties
+    });
+  }, (e) => {
+    res.status(400).send(e);
   });
 });
 
-app.post('/properties', (req, res) => {
-  console.log(req.body);
+app.get('/properties/:id', (req, res) => {
+  var id = req.params.id;
 
-  res.send({
-    id: 1,
-    data: 'saved ok'
+  // validate id is valid
+  if (!ObjectID.isValid(id)) {
+    // 404 - empty
+    return res.status(400).send();
+  }
+
+  Property.findById(id).then((property) => {
+    if (property) {
+      return res.send({property});
+    }
+    return res.status(404).send();
+
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+});
+
+app.post('/properties', (req, res) => {
+  //console.log(req.body);
+
+  var body = _.pick(req.body, ['listing_id',
+                              'property_report_url',
+                              'description',
+                              'first_published_date',
+                              'num_floors',
+                              'num_bedrooms',
+                              'num_bathrooms',
+                              'listing_status',
+                              'status',
+                              'property_type',
+                              'price',
+                              'latitude',
+                              'longitude',
+                              'displayable_address',
+                              'outcode',
+                              'country',
+                              'image_url',
+                              'thumbnail_url']);
+
+  var property = new Property({
+    listing_id: body.listing_id,
+    property_report_url: body.property_report_url,
+    description: body.description,
+    first_published_date: body.first_published_date,
+    num_floors: body.num_floors,
+    num_bedrooms: body.num_bedrooms,
+    num_bathrooms: body.num_bathrooms,
+    listing_status: body.listing_status,
+    status: body.status,
+    property_type: body.property_type,
+    price: body.price,
+    latitude: body.latitude,
+    longitude: body.longitude,
+    displayable_address: body.displayable_address,
+    outcode: body.outcode,
+    country: body.country,
+    image_url: body.image_url,
+    thumbnail_url: body.thumbnail_url
+  });
+
+  property.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
   });
 
 });
 
 app.put('/properties/:id', (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
+  var id = req.params.id;
 
-  var body = _.pick(req.body, ['data']);
+  // validate id is valid
+  if (!ObjectID.isValid(id)) {
+    // 400 - empty
+    return res.status(400).send();
+  }
 
-  console.log(body);
+  var body = _.pick(req.body, ['property_report_url',
+                              'description',
+                              'first_published_date',
+                              'num_floors',
+                              'num_bedrooms',
+                              'num_bathrooms',
+                              'listing_status',
+                              'status',
+                              'property_type',
+                              'price',
+                              'latitude',
+                              'longitude',
+                              'displayable_address',
+                              'outcode',
+                              'country',
+                              'image_url',
+                              'thumbnail_url']);
 
-  res.send({
-    id: 1,
-    data: 'updated ok'
+  Property.findByIdAndUpdate(id, {$set: body}, {new: true}).then((property) => {
+    if (!property) {
+      return res.status(404).send();
+    }
+    res.send({property});
+
+  }).catch((e) => {
+    res.status(500).send();
   });
 
 });
@@ -54,21 +142,33 @@ app.delete('/properties/:id', (req, res) => {
   var id = req.params.id;
 
   // validate id is valid
-  // if (!ObjectID.isValid(id)) {
-  //   // 404 - empty
-  //   return res.status(404).send();
-  // }
-  res.send({
-    id,
-    data: 'deleted ok'
+  if (!ObjectID.isValid(id)) {
+    // 404 - empty
+    return res.status(400).send();
+  }
+
+  Property.findByIdAndRemove(id).then((property) => {
+    if (property) {
+      return res.send({property});
+    }
+    return res.status(404).send();
+
+  }).catch((e) => {
+    res.status(500).send();
   });
 
 });
 
 app.post('/properties/zoopla/import', (req, res) => {
 
-  res.send({
-    data: 'updated everything from zoopla ok'
+  console.log(PropertyListingServices);
+
+  PropertyListingServices.importZooplaData().then(() => {
+    res.send({
+      data: 'updated everything from zoopla ok using promises'
+    });
+  }).catch((e) => {
+    console.log('in catch ', e);
   });
 
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,7 +1,26 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
-const {app}= require('./../server');
+const {app} = require('./../server');
+const {Property} = require('property-listing-models');
+
+const properties = [{
+  _id: new ObjectID(),
+  listing_id: '123',
+  description: 'first'
+}, {
+  _id: new ObjectID(),
+  listing_id: '456',
+  description: 'second'
+}];
+
+beforeEach((done) => {
+  Property.remove({}).then(() => {
+    return Property.insertMany(properties);
+  }).then(() => done());
+
+});
 
 describe('GET /properties', () => {
   it('should return all properties', (done) => {
@@ -10,7 +29,8 @@ describe('GET /properties', () => {
       .get('/properties')
       .expect(200)
       .expect((res) => {
-        expect(res.body.length).toBe(2);
+        expect(res.body.properties.length).toBe(2);
+        expect(res.body.properties[0].description).toBe('first');
       })
       .end(done);
 
@@ -20,36 +40,70 @@ describe('GET /properties', () => {
 describe('GET /properties/id', () => {
   it('should return a property', (done) => {
 
-    var id = 456;
+    var id = properties[0]._id.toHexString();
 
     request(app)
       .get(`/properties/${id}`)
       .expect(200)
       .expect((res) => {
-        expect(res.body.id).toEqual(id);
+        expect(res.body.property._id).toEqual(id);
+        expect(res.body.property.description).toEqual('first');
       })
       .end(done);
 
   });
+
+  it('should return a 400 if id is invalid format', (done) => {
+
+    var id = 123;
+
+    request(app)
+      .get(`/properties/${id}`)
+      .expect(400)
+      .end(done);
+
+  });
+
+  it('should return a 404 if id is not found', (done) => {
+
+    var id = '58bdf86abbd45802cb111fb1';
+
+    request(app)
+      .get(`/properties/${id}`)
+      .expect(404)
+      .end(done);
+
+  });
+
 });
 
 describe('POST /properties', () => {
   it('should save a property', (done) => {
 
-    var data = 'qwe';
+    var descText = 'third';
+
+    var data = {
+      description: descText,
+      num_floors: '2'
+    };
 
     request(app)
       .post('/properties')
-      .send({data})
+      .send(data)
       .expect(200)
       .expect((res) => {
-        expect(res.body.data).toBe('saved ok');
+        expect(res.body.description).toBe(descText);
       })
       .end((err, res) => {
         if (err) {
           return done(err);
         }
-        return done();
+
+        Property.find().then((properties) => {
+          expect(properties.length).toBe(3);
+          expect(properties[2].description).toBe(descText);
+          done();
+        }).catch((e) => done(e));
       });
   });
 });
@@ -57,15 +111,24 @@ describe('POST /properties', () => {
 describe('PUT /properties', () => {
   it('should update a property', (done) => {
 
-    var data = 'qwe';
-    var id = 123;
+    var property = {
+      description: 'some dataz',
+      listing_id: '789',
+      outcode: 'LS6',
+      foo: 'bar'
+    };
+    var id = properties[0]._id.toHexString();
 
     request(app)
       .put(`/properties/${id}`)
-      .send({data})
+      .send(property)
       .expect(200)
       .expect((res) => {
-        expect(res.body.data).toBe('updated ok');
+        expect(res.body.property.description).toBe('some dataz');
+        // not updatable via this endpoint
+        expect(res.body.property.listing_id).toBe('123');
+        expect(res.body.property.outcode).toBe('LS6');
+        expect(res.body.property.foo).toNotExist();
       })
       .end((err, res) => {
         if (err) {
@@ -74,26 +137,75 @@ describe('PUT /properties', () => {
         return done();
       });
   });
+
+  it('should return a 400 if id is invalid format', (done) => {
+
+    var id = 123;
+
+    request(app)
+      .put(`/properties/${id}`)
+      .expect(400)
+      .end(done);
+
+  });
+
+  it('should return a 404 if id is not found', (done) => {
+
+    var id = '58bdf86abbd45802cb111fb1';
+
+    request(app)
+      .put(`/properties/${id}`)
+      .expect(404)
+      .end(done);
+
+  });
+
 });
 
 describe('DELETE /properties/:id', () => {
   it('should remove a property', (done) => {
     //var hexId = todos[1]._id.toHexString();
-    var hexId = 555;
+    var hexId = properties[1]._id.toHexString();;
 
     request(app)
       .delete(`/properties/${hexId}`)
       .expect(200)
       .expect((res) => {
-        expect(res.body.id).toEqual(hexId);
+        expect(res.body.property._id).toEqual(hexId);
       })
       .end((err, res) => {
         if (err) {
           return done(err);
         }
-        return done();
+        Property.find().then((properties) => {
+          expect(properties.length).toBe(1);
+          done();
+        }).catch((e) => done(e));
       });
   });
+
+  it('should return a 400 if id is invalid format', (done) => {
+
+    var id = 123;
+
+    request(app)
+      .delete(`/properties/${id}`)
+      .expect(400)
+      .end(done);
+
+  });
+
+  it('should return a 404 if id is not found', (done) => {
+
+    var id = '58bdf86abbd45802cb111fb1';
+
+    request(app)
+      .delete(`/properties/${id}`)
+      .expect(404)
+      .end(done);
+
+  });
+
 });
 
 describe('POST /properties/zoopla/import', () => {
